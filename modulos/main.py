ed68f5e140029fe22570914149368df3313469f3
Jackson Lorran <computadordb@gmail.com>
2023-10-10 14:20:11 -0300

    "Adicionando modulo"

---
+++ modulos/main.py
@@ -0,0 +1,7 @@
+'''Modulação é importante quando queremos dividir as responsabildiades
+da aplicação. Assim se o sistema crescer vamos apenas acessar um certo
+local e fazer as alterações ou adicões'''
+import boas_vindass
+
+boas_vindass.msg_boas_vindas()
+boas_vindass.msg_usuario('ze')
