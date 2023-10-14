843332f519e777bba864446de504d9acf5c521b0
Jackson Lorran <computadordb@gmail.com>
2023-10-14 11:43:38 -0300

    Expressoes ternarias

---
+++ expressoes_ternarias/expressoes_ternarias.py
@@ -0,0 +1,8 @@
+#Expressoes tem uma funcionalidade parecida com comprehensions
+#entretanto utilizando condicionais
+
+num = -1
+
+#Aqui temos um teste para saber se a variavel 'num' é maior que 0
+#caso sim: printa positivo ,  caso não: printa negativo
+print('positivo' if num >= 0 else 'negativo') #positivo
