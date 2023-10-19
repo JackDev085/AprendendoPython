5d76f36289c328ec87270c6c4d917905a4fe7592
Jackson Lorran <computadordb@gmail.com>
2023-10-19 13:53:10 -0300

    Numpy finals

---
+++ Numpy/trabalhando com arrays/outros tópicos envolvendo arrays/subtração_de_arrays.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+#è possivel fazer a soma de arrays seguindo a lógica que os dois array necessitam ter o mesmo número de elementos
+
+d1 = np.array([3,6,3,6,1,2])
+d2 = np.array([3,6,3,6,1,2])
+
+d3 = d1+d2
+print(d3)#[ 6 12  6 12  2  4]
