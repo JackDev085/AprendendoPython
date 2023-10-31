e925c75023a46f7baeb2ef69d3fc7ee0550cb203
Jackson Lorran <computadordb@gmail.com>
2023-10-31 11:49:52 -0300

    Estrutura de dados

---
+++ estrutura de dados/listas.py
@@ -0,0 +1,7 @@
+#Ex de listas
+#As listas tendem a ser dinâmicas e homogêneas
+
+lista = []# Criando lista vazia
+lista = [1,2,3] # Criando lista de inteiros
+lista = [1, 'ola', 1.5] #Criando lista com vários tipos
+lista = [1, ['olá'], ['outra lista']] #Criando listas dentro de listas
---
+++ estrutura de dados/tuplas.py
@@ -0,0 +1,7 @@
+#Tuplas diferentes de listas não podem ser altradas
+
+tupla =(1,2,3) #Tupla de inteiros
+tupla= (1,'ola',1.5)#Tupla heterogênea
+
+#Maior desempenho
+#Menor alteração
